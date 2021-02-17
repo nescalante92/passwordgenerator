d8941ad5f615d10d91eb18982ebd6355e17a5ca3
Nelson Escalante <nescalante92@gmail.com>
2021-02-16 23:21:28 -0500

    final push added prompts alerts confrim

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # passwordgenerator
+
+## Description
+The task is to create a password generator that will allow users to select uppercase letters, lowercase letters, numbers or symbols to be inputed and then converted into a new paswword. This password generator completes it's task through the use of prompts, confirmations and alerts in the browser. 
+
+------------------------------------------------------------------
+
+## User Story
+An employee/employer would like the option to generate a secure random password that meets certain criterias in order to provide better  security regarding sensitive information.
+
+-----------------------------------------------------------------
+
+## Acceptance Criteria 
+- When the generate button is clicked, a series of prompts for password criteria is presented.
+
+- When prompts appear, the User can select which criteria to include within their password.
+
+- When prompted for the length of the password, the User can choose between the length of 8-128 characters to select.
+
+- When prompted for character types to be include in the password,
+the User can select an option between lowercase, uppercase, numeric, and/or special characters.
+
+- When each prompt is answered,the choosen input(s) should be validated and at least one character type should be selected 
+
+- When all prompts are answered, a password is generated that matches the selected criteria
+
+- When the new password is generated, the new password is either displayed in an alert or written to the page.
+
+--------------------------------------------------------------------
+
+## Links
+
+
+## Mockup Image
+![passwordgenerator](assets\images\passwordgenerator.jpg)
--- script.js
+++ script.js
@@ -1,131 +1,113 @@
-// The constant variables apply to the DOM and connect to the input functions
-// This includes the password textbox,checkboxes and the generate button
-const passwordEl = document.getElementById('#passwordinput')
-const characterEl = document.getElementById('#includeCharacter');
-const upperEl = document.getElementById('#includeUpper');
-const lowerEl = document.getElementById('#includeLower');
-const numberEl = document.getElementById('#includeNumber');
-const symbolEl = document.getElementById('#includeSymbol');
-const generateBtn = document.getElementById('#generate');
 
-//var randomObject = groups all the Upper/Lower/Number/Symbol Functions together
-//I researched how to use Character Codes to reduce amount of Arrays I need and to keep code cleaner
-// Math.random ()= generates a random rumber within perameter
-//Math.floor ()= helps to round numbers
+// The var generateBtn applies to the DOM and connects to the generate button
+var generateBtn = document.querySelector("#generate");
+var finalInput = document.querySelector("#password");
 
-// var randomObject ={
-//   upper: getUpper,
-//   lower: getLower,
-//   number: getNumber,
-//   symbol: getSymbol,
-// };
+//Add variables to create the arrays needed to input the characters into password
+//.split allows a segment from the array to be taken
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split();
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split();
+var numberCase = "0123456789".split();
+var symbolCase = "!@#$%^&*(){}[]<>?*".split();
 
 
+//Promt allows UI to receive the first message and starts character input process
+//Create generateFunction() to connect to writePassword() 
 
+function generatePassword() {
+  var finalInput = prompt("Please choose a Character length between 8-128");
+  
+  if (finalInput < 8 || finalInput > 128) {
+    alert("Please choose a Character length between 8-128");
+    return;
+  }
+  //Confrim allows UI to make a choice between yes and no for their input
+  var upperConfirm = confirm("Would you like to include Uppercase Letters?");
+  var lowerConfirm = confirm("Would you like to include Lowercase Letters?");
+  var numberConfirm = confirm("Would you like to include Numbers?");
+  var symbolConfirm = confirm("Would you like to include Symbols?");
+  console.log(upperConfirm, lowerConfirm, numberConfirm, symbolConfirm)
+  
+  //If statement confirms UI choices as true or false
+  //Alerts allow UI to to get a message and help confirm their choice
+  var finalPassword = "";
 
-//Add eventListender to respond to when specific items are clicked on or selected
+  if (upperConfirm === true) {
+    finalPassword = finalPassword.concat(upperCase);
+    alert("Uppercase Letters have been added");
+    
+  }
+  if (lowerConfirm === true) {
+    finalPassword = finalPassword.concat(lowerCase);
+    alert("Lowercase Letters have been added");
+    
+  }
+  if (numberConfirm === true) {
+    finalPassword = finalPassword.concat(numberCase);
+    alert("Numbers have been added");
+    
+  }
+  if (symbolConfirm === true) {
+    finalPassword = finalPassword.concat(symbolCase);
+    alert("Symbols have been added");
+    
+  }
+  console.log(finalPassword);
 
+  //For loop helps to generte random character selections and adds them together
+  
+  var newPassword = "";
+  for (var i = 0; i < finalInput; i++) {
+    newPassword = newPassword + finalPassword[Math.floor(Math.random() * finalPassword.length)]
+  }
+  console.log(newPassword);
+  return newPassword;
 
+};
 
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
+  passwordText.value = password;
 
-
-
-
-
-//Functions provide the range of letter/number/symbol values that that will be randomly produced
-function getUpper() {
-  const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  return upper[Math.floor(Math.random() * upper.length)];
 }
-  console.log(getUpper());
-
-
-
-function getLower() {
-  const lower = 'abcdefghijklmnopqrstuvwxyz';
-  return lower[Math.floor(Math.random() * lower.length)];
-}
-  console.log(getLower());
-
-
-function getNumber() {
-  const number = '0123456789';
-  return number[Math.floor(Math.random() * number.length)];
-}
-  console.log(getNumber());
-
-
-
-function getSymbol() {
-  const symbol = '!@#$%^&*(){}[]=<>';
-  return symbol[Math.floor(Math.random() * symbol.length)];
-}
-  console.log(getSymbol());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function writePassword() {
-//   if (generateBtn)
-//   console.log("password textbox is working");
-
-
-
-//   const password = generatePassword();7
-
-//   passwordText.value = password;
-
-
-// Loop to ranomly choose password
-
-
-
-
-// I researched how to add character codes to keep codes instead of using an array
-
-
 
 
 // Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
